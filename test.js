3c49dc9ff9acd8af822fe32796f5378bc3158741
Julian <julianferrari3@gmail.com>
2022-08-31 20:04:57 -0300

    Desafio Complementario

--- test.js
+++ test.js
@@ -70,30 +70,29 @@ function dolarizar(lista){
     lista.precio = parseFloat(lista.precio.toFixed(2));
 }
 
-// Funciones para mostrar los servicios.
-function show_servicios(lista){
+function get_servicios(lista){
     let lista_aux = []; 
     lista.forEach(function(item){ //Para que no muestre oficios repetidos.
         if(!lista_aux.includes(item.oficio)){
             lista_aux.push(item.oficio);
         }
     })
-    let temp = "Los servicios disponibles son: \n";
-    for(let x=0; x<lista_aux.length; x++){
-        temp += lista_aux[x] + " \n";
-    }
-    alert(temp);
+    return lista_aux
 }
 
-function show_servicios_disponibles(lista){
-    temp = "Los servicios disponibles son: \n";
-    for(let x=0; x<lista.length; x++){
-        temp += x + "- " + lista[x].nombre + " " + lista[x].apellido + " - $ " + lista[x].precio + " - Calificación: " + lista[x].calificacion+"\n"; 
+function mostrar_prestadores(){
+    disponibles_list = document.getElementById("disponibles_list");
+    for(let x=0; x<servicio_disponible.length; x++){
+        let li = document.createElement("li");
+        li.innerHTML = `<li>${x} - ${servicio_disponible[x].nombre} 
+                            ${servicio_disponible[x].apellido}
+                        - $ ${servicio_disponible[x].precio}
+            - calificación: ${servicio_disponible[x].calificacion}</li>`
+        disponibles_list.append(li); 
     }
-    alert(temp);
 }
 
-function show_servicio_adquirido(item, envio, pago){
+function calcular_precio(item, envio, pago){
     let precio = item.precio;
     let recargo = parseFloat(parseFloat(recargos(precio, envio, pago)).toFixed(2));
     let total = parseFloat(parseFloat(calcular(precio, recargo)).toFixed(2));
@@ -103,7 +102,7 @@ function show_servicio_adquirido(item, envio, pago){
     temp += "Se añaden $ " + recargo + " de recargo por el envío y el medio de pago elegido.\n";
     temp += "En total usted paga $ " + total;
 
-   alert(temp);
+   return temp
 }
 
 function ordenar_precio(a,b){
@@ -114,42 +113,63 @@ function ordenar_precio(a,b){
 // ----------------------------------------------------------
 // ----------------------------------------------------------
 
+let nombre = document.getElementById("nombre_usuario");
+
 let lista_servicios = generar_servicios(100); //Genero una lista de servicios aleatoria.
 //console.log(lista_servicios[1].calificacion);
 //lista_servicios[1].get_presentacion();
 //lista_servicios[1].set_presentacion("Soy un profesional matriculado etc...");
 //lista_servicios[1].get_presentacion();
 
+let servicios_list = document.getElementById("servicios_list");
+let lista_aux = get_servicios(lista_servicios);
+for(let x=0; x<lista_aux.length; x++){
+    let li = document.createElement("li");
+    li.innerHTML = `<li>${lista_aux[x]}</li>`
+    servicios_list.append(li);
+}
 
-show_servicios(lista_servicios);
-let servicio = prompt("¿Qué servicio quiere contratar?:");
-
-let servicio_existe = lista_servicios.find(servicios => servicios.oficio == servicio);
-console.log(servicio_existe);
-if (servicio_existe){
-    let servicio_disponible = lista_servicios.filter(servicios => servicios.oficio == servicio);
-    //console.log(servicio_disponible)
-
-    let ordenamiento = parseInt(prompt("Ordenar a los prestadores de servicios por [0- Ninguna / 1-Precio / 2-Calificación]:"));
-    if (ordenamiento==1){
-        servicio_disponible.sort(ordenar_precio);
-    }else if(ordenamiento==2){
-        servicio_disponible.sort((a,b) => parseInt(b.calificacion) - parseInt(a.calificacion));
-    }
+let servicio_disponible;
+let boton_servicio = document.getElementById("boton_servicio");
+boton_servicio.addEventListener("click", function(){
+    let servicio = document.getElementById("servicio");
+    //console.log(servicio.value);
+   
+    let servicio_existe = lista_servicios.find(servicios => servicios.oficio == servicio.value);
     
-    show_servicios_disponibles(servicio_disponible);
+    if (servicio_existe){
+        servicio_disponible = lista_servicios.filter(servicios => servicios.oficio == servicio.value);
     
-    if((prompt("¿Quiere ver los precios en dolares? [Y/N]:")) == "Y"){
-        servicio_disponible.map(dolarizar);
-        console.log(servicio_disponible);
-        show_servicios_disponibles(servicio_disponible);
-    }
+        if((prompt("¿Quiere ver los precios en dolares? [Y/N]:")) == "Y"){
+            servicio_disponible.map(dolarizar);
+            //console.log(servicio_disponible);
+        }
 
-    let eleccion = prompt("Elija a un prestador de servicio:"); 
+        let ordenamiento = parseInt(prompt("Ordenar a los prestadores de servicios por [0- Ninguna / 1-Precio / 2-Calificación]:"));
+        if (ordenamiento==1){
+            servicio_disponible.sort(ordenar_precio);
+        }else if(ordenamiento==2){
+            servicio_disponible.sort((a,b) => parseInt(b.calificacion) - parseInt(a.calificacion));
+        }
+
+        mostrar_prestadores();    
+    }else{
+        alert("Ese servicio no existe");
+    }
+})
+
+let resultado = document.getElementById("resultado");
+let boton_prestador = document.getElementById("boton_prestador");
+boton_prestador.addEventListener("click", function(){
+    let eleccion = document.getElementById("prestador");
     let envio = prompt("¿Necesita envío a domicilio? [Y/N]:");
     let pago = parseInt(prompt("Eliga el medio de pago: [1-Mercado Pago (0%) / 2-CBU (10%)"));
-    show_servicio_adquirido(servicio_disponible[eleccion], envio, pago); 
-}else{
-    alert("No tenemos el servicio solicitado.");
-    console.log("No tenemos el servicio solicitado.");
-}
+    let texto = calcular_precio(servicio_disponible[eleccion.value], envio, pago); 
+    console.log(texto);
+    resultado.innerHTML = texto;
+})
+
+
+
+
+
